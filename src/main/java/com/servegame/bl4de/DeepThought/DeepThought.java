01a3fd49c7f28361c3031337bea6de0e9a8ffc6e
Brandon Bires-Navel <brandonnavel@outlook.com>
2018-02-04 16:24:06 -0500

    Initial commit

---
+++ src/main/java/com/servegame/bl4de/DeepThought/DeepThought.java
@@ -0,0 +1,9 @@
+package com.servegame.bl4de.DeepThought;
+
+/**
+ * File: DeepThought.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class DeepThought {
+}
---
+++ src/main/java/com/servegame/bl4de/DeepThought/proxy/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * File: package-info.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+package com.servegame.bl4de.DeepThought.proxy;
