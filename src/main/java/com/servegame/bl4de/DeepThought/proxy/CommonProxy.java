da0f7b0b0ebc3cc2460718c6a746287f7b14640f
Brandon Bires-Navel <brandonnavel@outlook.com>
2018-02-04 16:34:36 -0500

    Added blank proxy classes, Client and DedicatedServer proxy extends the abstract CommonProxy

---
+++ src/main/java/com/servegame/bl4de/DeepThought/proxy/CommonProxy.java
@@ -0,0 +1,13 @@
+package com.servegame.bl4de.DeepThought.proxy;
+
+/**
+ * File: CommonProxy.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public abstract class CommonProxy {
+
+    public static final String CLIENT = "com.servegame.bl4de.DeepThought.proxy.ClientProxy";
+    public static final String SERVER = "com.servegame.bl4de.DeepThought.proxy.DedicatedServerProxy";
+
+}
