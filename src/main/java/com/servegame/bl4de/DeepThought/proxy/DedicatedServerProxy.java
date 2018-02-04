1e79fb200cc5a98742149446b298c85010fce85c
Brandon Bires-Navel <brandonnavel@outlook.com>
2018-02-04 16:47:03 -0500

    Created skeleton methods that need to be implemented from the abstract CommonProxy class

--- src/main/java/com/servegame/bl4de/DeepThought/proxy/DedicatedServerProxy.java
+++ src/main/java/com/servegame/bl4de/DeepThought/proxy/DedicatedServerProxy.java
@@ -1,9 +1,37 @@
 package com.servegame.bl4de.DeepThought.proxy;
 
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
 /**
  * File: DedicatedServerProxy.java
  *
  * @author Brandon Bires-Navel (brandonnavel@outlook.com)
  */
 public class DedicatedServerProxy extends CommonProxy {
+    @Override
+    public void init(FMLPreInitializationEvent event) {
+        // TODO
+    }
+
+    @Override
+    public void init(FMLInitializationEvent event) {
+        // TODO
+    }
+
+    @Override
+    public void init(FMLPostInitializationEvent event) {
+        // TODO
+    }
+
+    @Override
+    public boolean isDedicatedServer() {
+        return true;
+    }
+
+    @Override
+    protected void onTick() {
+        ++this.serverTickCount;
+    }
 }
